eb782de09104b34ae604e415810c743d5436ddd1
amandasgonsalves <105944287+amandasgonsalves@users.noreply.github.com>
2024-04-17 15:31:54 -0300

    atualizada

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -1,21 +1,59 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostChase : GhostBehavior
 {
-    public Ghost reference;
 
+    public Ghost reference;
     private void OnDisable()
     {
         ghost.scatter.Enable();
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        ChaseLogic(other);
+        Chase(other);
     }
 
-    private void ChaseLogic(Collider2D other)
+
+
+    public void Chase(Collider2D other)
+    {
+        switch (ghost.name)
+        {
+            case "Ghost_Blinky":
+                BlinkyGulosa(other);
+                break;
+            case "Ghost_Pinky":
+                MovimentaPinky(other);
+                break;
+            case "Ghost_Inky":
+                MovimentaInky(other);
+                break;
+            case "Ghost_Clyde":
+                MovimentaClyde(other);
+                break;
+        }
+    }
+
+    private void BlinkyGulosa(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && enabled && !ghost.frightened.enabled)
+        {
+            // Calcula a distância euclidiana entre o fantasma e o jogador
+            float distanceToTarget = Vector2.Distance(transform.position, ghost.target.position);
+
+            // Calcula a direção para o jogador (Pac-Man)
+            Vector2 directionToTarget = (ghost.target.position - transform.position).normalized;
+
+            // Define a direção de movimento do fantasma baseada na direção para o jogador
+            ghost.movement.SetDirection(directionToTarget);
+        }
+    }
+
+    private void Heuristica(Collider2D other, Func<Vector3, Vector3, Vector3> calculateTargetPosition)
     {
         Node node = other.GetComponent<Node>();
 
@@ -26,128 +64,53 @@ public class GhostChase : GhostBehavior
             List<Vector2> availableDirections = new List<Vector2>(node.availableDirections);
             availableDirections.Remove(ghost.movement.direction * -1);
 
-            switch (ghost.name)
+            foreach (Vector2 availableDirection in availableDirections)
             {
-                case "Ghost_Blinky":
-                    direction = ChaseDefault(other);
-                    break;
-                case "Ghost_Pinky":
-                    direction = ChaseFront(other);
-                    break;
-                case "Ghost_Inky":
-                    direction = ChaseTangent(other);
-                    break;
-                case "Ghost_Clyde":
-                    direction = ChaseRadius(other);
-                    break;
+                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f);
+                Vector3 targetPosition = calculateTargetPosition(newPosition, ghost.target.position);
+
+                float distance = (targetPosition - newPosition).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    direction = availableDirection;
+                }
             }
 
             ghost.movement.SetDirection(direction);
         }
     }
 
-    private Vector2 ChaseFront(Collider2D other)
+    private void MovimentaPinky(Collider2D other)
     {
-        Vector2 direction = Vector2.zero;
-        float minDistance = float.MaxValue;
-        Node node = other.GetComponent<Node>();
-
-        foreach (Vector2 availableDirection in node.availableDirections)
-        {
-            if (availableDirection != -ghost.movement.direction)
-            {
-                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f);
-                Vector3 frontOfTarget = ghost.target.position + new Vector3(ghost.target.GetComponent<Movement>().direction.x * 6f, ghost.target.GetComponent<Movement>().direction.y * 6f, 0f);
-                float distance = (frontOfTarget - newPosition).sqrMagnitude;
-
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    direction = availableDirection;
-                }
-            }
-        }
-
-        return direction;
+        Heuristica(other, (newPosition, targetPosition) => {
+            return targetPosition + new Vector3(ghost.target.GetComponent<Movement>().direction.x * 6f, ghost.target.GetComponent<Movement>().direction.y * 6f, 0f);
+        });
     }
 
-    private Vector2 ChaseTangent(Collider2D other)
+    private void MovimentaInky(Collider2D other)
     {
-        Vector2 direction = Vector2.zero;
-        float minDistance = float.MaxValue;
-        Node node = other.GetComponent<Node>();
-
-        if (reference != null)
-        {
-            foreach (Vector2 availableDirection in node.availableDirections)
-            {
-                if (availableDirection != -ghost.movement.direction)
-                {
-                    Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f);
-                    Vector3 frontOfTarget = ghost.target.position + new Vector3(ghost.target.GetComponent<Movement>().direction.x * 6f, ghost.target.GetComponent<Movement>().direction.y * 6f, 0f);
-                    float xDistance = frontOfTarget.x - reference.transform.position.x;
-                    float yDistance = frontOfTarget.y - reference.transform.position.y;
-                    Vector3 point = new Vector3(frontOfTarget.x + xDistance, frontOfTarget.y + yDistance, 0f);
-                    float distance = (point - newPosition).sqrMagnitude;
-
-                    if (distance < minDistance)
-                    {
-                        minDistance = distance;
-                        direction = availableDirection;
-                    }
-                }
-            }
-        }
-
-        return direction;
+        Heuristica(other, (newPosition, targetPosition) => {
+            float xDistance = targetPosition.x - reference.transform.position.x;
+            float yDistance = targetPosition.y - reference.transform.position.y;
+            return new Vector3(targetPosition.x + xDistance, targetPosition.y + yDistance, 0f);
+        });
     }
 
-    private Vector2 ChaseDefault(Collider2D other)
+    private void MovimentaClyde(Collider2D other)
     {
-        Vector2 direction = Vector2.zero;
-        float minDistance = float.MaxValue;
-        Node node = other.GetComponent<Node>();
-
-        foreach (Vector2 availableDirection in node.availableDirections)
-        {
-            if (availableDirection != -ghost.movement.direction)
+        Heuristica(other, (newPosition, targetPosition) => {
+            if (Vector3.Distance(targetPosition, newPosition) < 8f)
             {
-                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f);
-                float distance = (ghost.target.position - newPosition).sqrMagnitude;
-
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    direction = availableDirection;
-                }
+                return ghost.home.transform.position;
             }
-        }
-
-        return direction;
+            else
+            {
+                return targetPosition;
+            }
+        });
     }
 
-    private Vector2 ChaseRadius(Collider2D other)
-    {
-        Vector2 direction = Vector2.zero;
-        float minDistance = float.MaxValue;
-        Node node = other.GetComponent<Node>();
 
-        foreach (Vector2 availableDirection in node.availableDirections)
-        {
-            if (availableDirection != -ghost.movement.direction)
-            {
-                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f);
-                float distanceToPacman = Vector3.Distance(ghost.target.position, newPosition);
-                float distance = (distanceToPacman < 8f) ? (ghost.home.transform.position - newPosition).sqrMagnitude : (ghost.target.position - newPosition).sqrMagnitude;
-
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    direction = availableDirection;
-                }
-            }
-        }
-
-        return direction;
-    }
 }
